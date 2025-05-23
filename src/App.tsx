a7054089ce7d69a0274e597f1eaaaf8f5424f710
Camila Machado <camila.maia.1205600@sga.pucminas.br>
2025-05-23 15:33:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStack from './src/navigation/MainStack';
 import './src/firebase/firebase';
+import { ThemeProvider } from './src/context/Modo_Claro';
 
 export default function App() {
   return (
+     <ThemeProvider>
     <NavigationContainer>
       <MainStack />
     </NavigationContainer>
+    </ThemeProvider>
   );
 }
