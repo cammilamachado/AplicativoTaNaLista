63873718ffbad6737b5db184dc3431632b2c0aed
Camila Machado <camila.maia.1205600@sga.pucminas.br>
2025-05-23 14:11:34 -0300

    Novas rotas

--- src/src/navigation/MainStack.tsx
+++ src/src/navigation/MainStack.tsx
@@ -4,12 +4,25 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaInicial from '../screens/TelaInicial';
 import Cadastro from '../screens/Cadastro';
 import Login from '../screens/Login';
+import Home from '../screens/Home';
 import MyTabs from './TabsLista';
+import Config from '../screens/Config';
+import Gastos from '../screens/Gastos';
+import Ajuda from '../screens/Ajuda';
+import Sobre from '../screens/Sobre';
+import Suporte from '../screens/Suporte';
+
 export type RootStackParamList = {
   Inicio: undefined;
   Cadastro: undefined;
   Login: undefined;
   NavLista: undefined;
+  Home: undefined;
+  Config: undefined;
+  Gastos: undefined;
+  Ajuda: undefined;
+  Sobre: undefined;
+  Suporte: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,6 +34,12 @@ export default function MainStack() {
       <Stack.Screen name="Cadastro" component={Cadastro} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="NavLista" component={MyTabs} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Gastos" component={Gastos} />
+      <Stack.Screen name="Ajuda" component={Ajuda} />
+      <Stack.Screen name="Sobre" component={Sobre} />
+      <Stack.Screen name="Config" component={Config} />
+      <Stack.Screen name="Suporte" component={Suporte} />
     </Stack.Navigator>
   );
 }
